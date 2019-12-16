337fb7083ded7ddd9f74eaad24c43b7a98472a0c
mengxue.zmx <mengxue.zmx@alibaba-inc.com>
2019-12-16 12:06:52 +0800

    add:PPT

---
+++ PPT/Node.js加速Qzone/window/Window.js
@@ -0,0 +1,25 @@
+﻿var domain = require('domain');
+
+function Window(){
+	this.domain			= domain.create();
+	this.domain.window	= this;
+}
+
+Window.prototype.run = function(fn){
+	this.domain.run(fn);
+}
+
+module.exports = Window;
+
+Object.defineProperty(global, 'window', {
+    get : function(){
+        return process.domain && process.domain.window;
+    }
+});
+
+
+
+
+
+
+
---
+++ PPT/Node.js加速Qzone/window/demo.js
@@ -0,0 +1,22 @@
+﻿var Window = require('./Window');
+
+var win1 = new Window();
+var win2 = new Window();
+
+win1.request = 'request-1';
+win2.request = 'request-2';
+
+win1.run(api);
+win2.run(api);
+
+
+function api(){
+	console.log(window.request);
+	
+	setTimeout(function(){
+		console.log(window.request);	
+	},500);
+}
+
+
+
